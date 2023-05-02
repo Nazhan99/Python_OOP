026bab4eb076491b9fd6e4b1228d1c29c361f14c
Nazhan Hakeem <57025187+Nazhan99@users.noreply.github.com>
2023-05-02 21:53:51 +0800

    Add files via upload

---
+++ Class_Attribute.py
@@ -0,0 +1,24 @@
+class ClassName:
+
+#Class Attributes
+#__init__()
+#Methods
+pass
+
+class Dog:
+
+    species = "Canis lupus"
+
+    def __int__(self, name, age, breed):
+        self.name = name
+        self.age = age
+        self.breed = breed
+pass
+
+class Backpack:
+
+    max_num_items = 10
+
+    def __int__(self):
+        self.items = []
+
