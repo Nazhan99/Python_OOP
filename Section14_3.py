714366c621ee826e8645dddfaeeca34f19273891
Nazhan Hakeem <57025187+Nazhan99@users.noreply.github.com>
2023-06-06 20:43:55 +0800

    Add files via upload

---
+++ Section14_3.py
@@ -0,0 +1,23 @@
+class Professor:
+    def __init__(self, name, age, course):
+        self.name = name
+        self.age = age
+        self.course = course
+
+    def greet_students(self):
+        print("Welcome to class!")
+
+
+# Define your class below this line
+
+class ScienceProfessor(Professor):
+
+    def __init__(self, name, age, course):
+        Professor.__init__(self, name, age, course)
+
+    def greet_students(self):
+        print("Hi everyone! It's a great day to study Science!")
+        Professor.greet_students(self)
+
+
+science_professor = ScienceProfessor("ali", 34, "SCIENCE")
