838e0727d1e7e9583530a798082aaa53421b11b5
Nazhan Hakeem <57025187+Nazhan99@users.noreply.github.com>
2023-06-11 22:46:23 +0800

    Add files via upload

---
+++ Python_Exercises/Exercise33.py
@@ -0,0 +1,5 @@
+#Given the code below, use the correct logical operator in between the two expressions on line 1 in order for the final result to be False.
+
+result = ((25 % 7 + 10 / 2) % 3 == 0)  < ((abs(-19) / 2 - 2) > 9)
+
+print(result) #should return False
