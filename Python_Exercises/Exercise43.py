c09d2d8ebe474427a310b16760f407053829e176
Nazhan Hakeem <57025187+Nazhan99@users.noreply.github.com>
2023-06-17 22:17:58 +0800

    Add files via upload

---
+++ Python_Exercises/Exercise43.py
@@ -0,0 +1,7 @@
+#Given the code below, use the correct method on line 3 in order to concatenate my_list with [100, 101, 102], by adding the latter at the end of my_list.
+
+my_list = [10, 10.5, 20, 30, 'Python', 'Java', 'Ruby']
+
+my_list = my_list + [100, 101, 102]
+
+print(my_list)
