02bdc67a2e6912dfd749d44e26edf87952f7902e
Nazhan Hakeem <57025187+Nazhan99@users.noreply.github.com>
2023-06-27 23:23:27 +0800

    Add files via upload

---
+++ Python_Exercises/Exercise66.py
@@ -0,0 +1,7 @@
+#Given the code below, use the correct method on line 3 in order to delete the element 9 from my_set.
+
+my_set = {1, 4, 6, 5, 9, 0, 8, 3, 2, 7, 11}
+
+my_set.remove(9)
+
+print(my_set)
