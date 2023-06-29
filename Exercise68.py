b918147c345c92315bef31c4153429b6e30328bf
Nazhan Hakeem <57025187+Nazhan99@users.noreply.github.com>
2023-06-29 23:03:05 +0800

    Add files via upload

---
+++ Exercise68.py
@@ -0,0 +1,8 @@
+#Given the code below, use the correct method on line 4 in order to join the elements of my_set1 and my_set2.
+
+my_set1 = {1, 4, 6, 5, 9, 0, 8, 3, 2, 7, 11}
+my_set2 = {12, 9, 4, 2, 0, 6}
+
+join = my_set1.union(my_set2)
+
+print(sorted(list(join)))
