73fe823238845016706e60e68eb6bd17526c7db8
Nazhan Hakeem <57025187+Nazhan99@users.noreply.github.com>
2023-07-02 22:31:57 +0800

    Add files via upload

---
+++ Python_Exercises/Exercise71.py
@@ -0,0 +1,7 @@
+#Given the code below, use the correct method on line 3 in order to find out the index of Slovakia in my_tup.
+
+my_tup = ("Romania", "Poland", "Estonia", "Bulgaria", "Slovakia", "Slovenia", "Hungary")
+
+index = my_tup.index("Slovakia")
+
+print(index)
