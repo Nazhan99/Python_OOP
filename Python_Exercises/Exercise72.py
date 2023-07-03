efc89be48a37f338dd32446640e32749e32085e9
Nazhan Hakeem <57025187+Nazhan99@users.noreply.github.com>
2023-07-03 21:44:51 +0800

    Add files via upload

---
+++ Python_Exercises/Exercise72.py
@@ -0,0 +1,7 @@
+#Given the code below, write code in order to perform tuple assignment on line 3 and obtain the results below.
+
+my_tup = ("Romania", "Poland", "Estonia")
+
+(ro, po, es) = my_tup
+
+print(ro + ", " + po + ", " + es) #returns 'Romania, Poland, Estonia'
