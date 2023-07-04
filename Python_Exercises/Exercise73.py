e78b8bfbf7f4f1d3e18e8453fb0e92496fefffed
Nazhan Hakeem <57025187+Nazhan99@users.noreply.github.com>
2023-07-04 20:45:28 +0800

    Add files via upload

---
+++ Python_Exercises/Exercise73.py
@@ -0,0 +1,7 @@
+#Given the code below, use the correct method on line 3 in order to find out the last element of my_tup in alphabetical order.
+
+my_tup = ("Romania", "Poland", "Estonia", "Bulgaria", "Slovakia", "Slovenia", "Hungary")
+
+last = max(my_tup)
+
+print(last) #should return Slovenia
