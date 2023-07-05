bb846eb85cd212dcc493401a0900ccb78910d7fe
Nazhan Hakeem <57025187+Nazhan99@users.noreply.github.com>
2023-07-05 20:52:57 +0800

    Add files via upload

---
+++ Python_Exercises/Exercise74.py
@@ -0,0 +1,7 @@
+#Given the code below, use the correct method on line 3 in order to find out the number of occurrences of Estonia in my_tup.
+
+my_tup = ("Romania", "Poland", "Estonia", "Bulgaria", "Slovakia", "Slovenia", "Estonia", "Romania", "Hungary", "Slovenia")
+
+number = my_tup.count("Estonia")
+
+print(number)
