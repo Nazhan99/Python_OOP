472fcec15a657b935653ee7b32ec1a0324aeeb63
Nazhan Hakeem <57025187+Nazhan99@users.noreply.github.com>
2023-07-07 00:02:29 +0800

    Add files via upload

---
+++ Python_Exercises/Exercise77.py
@@ -0,0 +1,7 @@
+#Given the code below, use the correct slice on line 3 in order to return all the elements of my_tup, except the first three of them, using a positive index.
+
+my_tup = ("Romania", "Poland", "Estonia", "Bulgaria", "Slovakia", "Slovenia", "Hungary")
+
+my_slice = my_tup[3:]
+
+print(my_slice)
