70dc8db8d3addcab1c606420a65778525a70d640
Nazhan Hakeem <57025187+Nazhan99@users.noreply.github.com>
2023-07-06 21:36:49 +0800

    Add files via upload

---
+++ Python_Exercises/Exercise76.py
@@ -0,0 +1,7 @@
+#Given the code below, use the correct slice on line 3 in order to return all the elements of my_tup, except the last three of them, using a negative index.
+
+my_tup = ("Romania", "Poland", "Estonia", "Bulgaria", "Slovakia", "Slovenia", "Hungary")
+
+my_slice = my_tup[:-3]
+
+print(my_slice)
