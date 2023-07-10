05da2f7451ceb2bc754f5cf8b972d9066786b2f5
Nazhan Hakeem <57025187+Nazhan99@users.noreply.github.com>
2023-07-10 19:02:54 +0800

    Add files via upload

---
+++ Python_Exercises/Exercise81.py
@@ -0,0 +1,5 @@
+#Given the code below, use the correct argument(s) for the range() function on line 1 in order to return a range of consecutive integers from 117 to 129 exclusively.
+
+my_range = range(117, 129)
+
+print(list(my_range)) #[117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128]
