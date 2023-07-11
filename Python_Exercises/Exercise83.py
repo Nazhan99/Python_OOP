446c8c0bdfd9ee5dab1b26397406c224b401951f
Nazhan Hakeem <57025187+Nazhan99@users.noreply.github.com>
2023-07-11 22:35:49 +0800

    Add files via upload

---
+++ Python_Exercises/Exercise83.py
@@ -0,0 +1,5 @@
+#Given the code below, add the correct step as the third argument of the range() function on line 1 in order to return [115, 120] when converted to a list.
+
+my_range = range(115, 125, 5)
+
+print(list(my_range)) #[115, 120]
