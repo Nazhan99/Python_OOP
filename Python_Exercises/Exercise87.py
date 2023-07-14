ee216674572c628bef544d7abf5fb0a0a744f449
Nazhan Hakeem <57025187+Nazhan99@users.noreply.github.com>
2023-07-14 22:45:04 +0800

    Add files via upload

---
+++ Python_Exercises/Exercise87.py
@@ -0,0 +1,7 @@
+#Given the code below, use the correct code on line 3 in order to return the value associated with key 4. Do not use a method as a solution for this exercise!
+
+crypto = {1: "Bitcoin", 2: "Ethereum", 3: "Litecoin", 4: "Stellar", 5: "XRP"}
+
+value = crypto[4]
+
+print(value)
