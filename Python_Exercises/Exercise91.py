31a9003946e9ad5cabbcf91440c9b62db089a8bd
Nazhan Hakeem <57025187+Nazhan99@users.noreply.github.com>
2023-07-17 20:51:44 +0800

    Add files via upload

---
+++ Python_Exercises/Exercise91.py
@@ -0,0 +1,7 @@
+#Given the code below, use the correct code on line 3 in order to return the number of key-value pairs in the dictionary.
+
+crypto = {1: "Bitcoin", 2: "Ethereum", 3: "Litecoin", 4: "Stellar", 5: "XRP"}
+
+number = len(crypto)
+
+print(number)
