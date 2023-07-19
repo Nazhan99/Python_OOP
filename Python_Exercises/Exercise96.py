8c80519cf5d0e4fb1981df3f6478899eb4e87157
Nazhan Hakeem <57025187+Nazhan99@users.noreply.github.com>
2023-07-19 10:47:32 +0800

    Add files via upload

---
+++ Python_Exercises/Exercise96.py
@@ -0,0 +1,7 @@
+#Given the code below, use the correct code on line 3 in order to get a list of tuples, where each tuple represents a key-value pair in the dictionary.
+
+crypto = {1: "Bitcoin", 2: "Ethereum", 3: "Litecoin", 4: "Stellar", 5: "XRP"}
+
+result = crypto.items()
+
+print(list(result))
