0abb78a58bff1647caba09773470ede0ed6dfab1
Nazhan Hakeem <57025187+Nazhan99@users.noreply.github.com>
2023-07-30 22:58:32 +0800

    Add files via upload

---
+++ Python_Exercises/Exercise107.py
@@ -0,0 +1,7 @@
+#Given the code below, use the correct function on line 3 in order to convert value to a frozen set.
+
+value = (10, 20, 40, 10, 25, 30, 45)
+
+conv = frozenset(value)
+
+print(type(conv))
