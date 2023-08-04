d3b5906e231c4df8d90d04be5b2e42f49c65eae9
Nazhan Hakeem <57025187+Nazhan99@users.noreply.github.com>
2023-08-04 23:07:58 +0800

    Add files via upload

---
+++ Python_Exercises/Exercise112.py
@@ -0,0 +1,6 @@
+#Considering the code below, write code that prints out True! if x has 50 characters or more.
+
+x = "The days of Python 2 are almost over. Python 3 is the king now."
+
+if len(x) >= 50:
+    print("True!")
