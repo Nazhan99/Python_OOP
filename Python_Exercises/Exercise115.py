40b9d206c9d7bc40d00dab6383eaaefcd9877412
Nazhan Hakeem <57025187+Nazhan99@users.noreply.github.com>
2023-08-07 22:24:41 +0800

    Add files via upload

---
+++ Python_Exercises/Exercise115.py
@@ -0,0 +1,8 @@
+#Considering the code below, write code that prints out True! if the index of the first occurrence of letter f is less than 10 and prints out False! if the same condition is not satisfied.
+
+x = "The days of Python 2 are almost over. Python 3 is the king now."
+
+if x.index("f") < 10:
+    print("True!")
+else:
+    print("False!")
