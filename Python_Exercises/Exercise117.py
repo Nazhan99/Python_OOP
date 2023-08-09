a296d0084633b408ce85521dfaddc43cc7fdc5e7
Nazhan Hakeem <57025187+Nazhan99@users.noreply.github.com>
2023-08-09 21:26:02 +0800

    Add files via upload

---
+++ Python_Exercises/Exercise117.py
@@ -0,0 +1,2 @@
+#Considering the code below, write code that prints out True! if x has at least 8 elements and the element positioned at index 6 is a floating-point number and prints out False! otherwise.
+
